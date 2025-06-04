f7a7997b5faf70dd0e022841e32af6bd3d9dc2e1
Houssam Bendour <hsmbndr1@gmail.com>
2025-06-04 09:35:53 +0100

    Create civ

---
+++ backend/civ/src/test/java/ma/project/civ/CivApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.project.civ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
